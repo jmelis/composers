aacbc517d4705196f38593139a30268d457264de
Jaime Melis <jmelis@redhat.com>
2022-02-25 23:29:04 +0100

    add composers

--- pages/index.js
+++ pages/index.js
@@ -2,170 +2,7 @@ import React, { useState } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Fuse from 'fuse.js'
-
-const LIST = [
-  {
-    "title": "Old Man's War",
-    "author": {
-      "firstName": "John",
-      "lastName": "Scalzi"
-    }
-  },
-  {
-    "title": "The Lock Artist",
-    "author": {
-      "firstName": "Steve",
-      "lastName": "Hamilton"
-    }
-  },
-  {
-    "title": "HTML5",
-    "author": {
-      "firstName": "Remy",
-      "lastName": "Sharp"
-    }
-  },
-  {
-    "title": "Right Ho Jeeves",
-    "author": {
-      "firstName": "P.D",
-      "lastName": "Woodhouse"
-    }
-  },
-  {
-    "title": "The Code of the Wooster",
-    "author": {
-      "firstName": "P.D",
-      "lastName": "Woodhouse"
-    }
-  },
-  {
-    "title": "Thank You Jeeves",
-    "author": {
-      "firstName": "P.D",
-      "lastName": "Woodhouse"
-    }
-  },
-  {
-    "title": "The DaVinci Code",
-    "author": {
-      "firstName": "Dan",
-      "lastName": "Brown"
-    }
-  },
-  {
-    "title": "Angels & Demons",
-    "author": {
-      "firstName": "Dan",
-      "lastName": "Brown"
-    }
-  },
-  {
-    "title": "The Silmarillion",
-    "author": {
-      "firstName": "J.R.R",
-      "lastName": "Tolkien"
-    }
-  },
-  {
-    "title": "Syrup",
-    "author": {
-      "firstName": "Max",
-      "lastName": "Barry"
-    }
-  },
-  {
-    "title": "The Lost Symbol",
-    "author": {
-      "firstName": "Dan",
-      "lastName": "Brown"
-    }
-  },
-  {
-    "title": "The Book of Lies",
-    "author": {
-      "firstName": "Brad",
-      "lastName": "Meltzer"
-    }
-  },
-  {
-    "title": "Lamb",
-    "author": {
-      "firstName": "Christopher",
-      "lastName": "Moore"
-    }
-  },
-  {
-    "title": "Fool",
-    "author": {
-      "firstName": "Christopher",
-      "lastName": "Moore"
-    }
-  },
-  {
-    "title": "Incompetence",
-    "author": {
-      "firstName": "Rob",
-      "lastName": "Grant"
-    }
-  },
-  {
-    "title": "Fat",
-    "author": {
-      "firstName": "Rob",
-      "lastName": "Grant"
-    }
-  },
-  {
-    "title": "Colony",
-    "author": {
-      "firstName": "Rob",
-      "lastName": "Grant"
-    }
-  },
-  {
-    "title": "Backwards, Red Dwarf",
-    "author": {
-      "firstName": "Rob",
-      "lastName": "Grant"
-    }
-  },
-  {
-    "title": "The Grand Design",
-    "author": {
-      "firstName": "Stephen",
-      "lastName": "Hawking"
-    }
-  },
-  {
-    "title": "The Book of Samson",
-    "author": {
-      "firstName": "David",
-      "lastName": "Maine"
-    }
-  },
-  {
-    "title": "The Preservationist",
-    "author": {
-      "firstName": "David",
-      "lastName": "Maine"
-    }
-  },
-  {
-    "title": "Fallen",
-    "author": {
-      "firstName": "David",
-      "lastName": "Maine"
-    }
-  },
-  {
-    "title": "Monster 1959",
-    "author": {
-      "firstName": "David",
-      "lastName": "Maine"
-    }
-  }
-];
+import Composers from './composers.js'
 
 export default function Home() {
   const [results, setResults] = useState([]);
@@ -185,13 +22,13 @@ export default function Home() {
     // ignoreFieldNorm: false,
     // fieldNormWeight: 1,
     keys: [
-      "title",
-      "author.firstName",
-      "author.lastName",
+      "name",
     ]
   };
   
-  const fuse = new Fuse(LIST, options);
+  const composers = Composers.map(e => ({'name': e[0], 'born': e[1], 'death': e[2], 'style': e[3]}));
+
+  const fuse = new Fuse(composers, options);
 
   function handleChange(event) {
     const pattern = event.target.value;
@@ -212,7 +49,7 @@ export default function Home() {
 
       <main className={styles.main}>
       <input type="text" onChange={handleChange} />
-      {results.map(e => <li>{JSON.stringify(e["item"])}</li>)}
+      {results.map(e => <li>{e['item'].name} ({e['item'].born} - {e['item'].death}) [{e['item'].style}]</li>)}
       </main>
     </div>
   )
